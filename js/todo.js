3267134bf731fb97b8da874b1222ad1147a74311
Richard Baker <richiebkr@gmail.com>
2014-04-25 10:47:45 -0700

    added styles for border

--- js/todo.js
+++ js/todo.js
@@ -1,27 +1,29 @@
 window.onload = function(){
-	
+
 	var entry = document.forms["todo_form"];
 	var button = document.getElementById('submit_todo');
-	
-    entry["todo_info"].onfocus = textDissapear;
+
+	entry["todo_info"].onfocus = textDissapear;
 	button.onclick = exchangeText;
-	
+
 	function textDissapear(){
 		this.value = "";
 		entry["todo_info"].style.color = "black";
 	}
-};
 
-function exchangeText(){
-	var entry = document.forms["todo_form"];
-		todoList = document.createElement("span");
-		valueOfToDo = entry["todo_info"].value;
-		entries = document.getElementsByTagName('span');
-	
-	entries.appendChild(todoList);
-    
-	var toDoEntry = document.getElementsByTagName('span');
-	toDoEntry.innerHTML = valueOfToDo.innerHTML;
-	document.getElementById('single_todo').innerHTML=valueOfToDo;
-	entry["todo_info"].value = '';
-}
+
+	function exchangeText(){
+		var entry = document.forms["todo_form"];
+			todoList = document.createElement("span");
+			valueOfToDo = entry["todo_info"].value;
+			entries = document.getElementsByTagName('span');
+
+		entries.appendChild(todoList);
+
+		var toDoEntry = document.getElementsByTagName('span');
+		toDoEntry.innerHTML = valueOfToDo.innerHTML;
+		document.getElementById('single_todo').innerHTML=valueOfToDo;
+		entry["todo_info"].value = '';
+	}
+
+};
